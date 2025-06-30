7b0ef7ba078c2dec3a05cac4e1a6ae5a4d11c165
João <devnogg.yamamoto@gmail.com>
2025-06-30 19:45:30 -0300

    Versão final

--- src/main/java/br/edu/ifb/java_web_dev/controller/CalculatorRestController.java
+++ src/main/java/br/edu/ifb/java_web_dev/controller/CalculatorRestController.java
@@ -1,5 +1,6 @@
 package br.edu.ifb.java_web_dev.controller;
 
+import br.edu.ifb.java_web_dev.model.CalculatorService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,53 +8,53 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class CalculatorRestController {
 
+    private final CalculatorService calculatorService = new CalculatorService();
+
     // Métodos GET
     @GetMapping("/sum")
-    public double sum(@RequestParam double a, @RequestParam double b) {
-        return a + b;
+    public int sum(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.sum(a, b);
     }
 
     @GetMapping("/sub")
-    public double sub(@RequestParam double a, @RequestParam double b) {
-        return a - b;
+    public int sub(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.sub(a, b);
     }
 
     @GetMapping("/mul")
-    public double mul(@RequestParam double a, @RequestParam double b) {
-        return a * b;
+    public int mul(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.mul(a, b);
     }
 
     @GetMapping("/div")
-    public double div(@RequestParam double a, @RequestParam double b) {
-        if (b == 0) throw new IllegalArgumentException("Divisão por zero não permitida");
-        return a / b;
+    public double div(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.div(a, b);
     }
 
     // Métodos POST
     @PostMapping("/sum")
-    public double sumPost(@RequestBody Operands operands) {
-        return operands.a + operands.b;
+    public int sumPost(@RequestBody Operands operands) {
+        return calculatorService.sum(operands.a, operands.b);
     }
 
     @PostMapping("/sub")
-    public double subPost(@RequestBody Operands operands) {
-        return operands.a - operands.b;
+    public int subPost(@RequestBody Operands operands) {
+        return calculatorService.sub(operands.a, operands.b);
     }
 
     @PostMapping("/mul")
-    public double mulPost(@RequestBody Operands operands) {
-        return operands.a * operands.b;
+    public int mulPost(@RequestBody Operands operands) {
+        return calculatorService.mul(operands.a, operands.b);
     }
 
     @PostMapping("/div")
     public double divPost(@RequestBody Operands operands) {
-        if (operands.b == 0) throw new IllegalArgumentException("Divisão por zero não permitida");
-        return operands.a / operands.b;
+        return calculatorService.div(operands.a, operands.b);
     }
 
     // Classe auxiliar para POST
     public static class Operands {
-        public double a;
-        public double b;
+        public int a;
+        public int b;
     }
 }
